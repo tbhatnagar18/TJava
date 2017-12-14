9008834c70c84e1239901cf35ea17dd82c85692d
tbhatn <tbhatn@dludclab03.sapient.com>
2017-12-15 01:00:46 +0530

    Refractoring names and packages

---
+++ AceDirectoryScanner/src/com/tjava/Scanner/FileTypes/CreateSMTD.java
@@ -0,0 +1,111 @@
+package com.tjava.Scanner.FileTypes;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+public class CreateSMTD implements Runnable {
+
+	String directory;
+	CountDownLatch latch;
+	String sortingCriteria;
+
+	public CreateSMTD(String directory, CountDownLatch latch, String sortingCriteria) {
+		super();
+		this.directory = directory;
+		this.latch = latch;
+		this.sortingCriteria = sortingCriteria;
+	}
+
+	@Override
+	public void run() {
+
+		List<File> files = CommonUtilities.getAllMTDFiles(directory);
+
+		List<String> fileNameList = generateFileListBasedOnCriteria(files);
+
+		try {
+			createAndWriteToSMTDFile(fileNameList, directory);
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		finally {
+
+			latch.countDown();
+		}
+	}
+
+	private void createAndWriteToSMTDFile(List<String> fileNameList, String directory2)
+			throws FileNotFoundException, UnsupportedEncodingException {
+
+		directory2 = directory2 + directory2.substring(directory2.lastIndexOf("\\"));
+
+		PrintWriter writer = new PrintWriter(directory2 + ".smtd", "UTF-8");
+
+		for (String fileName : fileNameList) {
+			writer.println(fileName);
+
+		}
+		writer.close();
+
+	}
+
+	// generating list of sorted file names based on criteria
+	private List<String> generateFileListBasedOnCriteria(List<File> files) {
+
+		List<String> list = new ArrayList<String>();
+
+		// creating map for Filename and metadata value to be sorted on.
+		Map<String, Integer> fileMap = new HashMap<String, Integer>();
+		try {
+			for (File file : files) {
+
+				int[] metadata = CommonUtilities.scanMTDFile(file);
+				int value = findMetadataForSortingCriteria(sortingCriteria, metadata);
+				fileMap.put(file.getAbsolutePath(), value);
+
+			}
+
+			System.out.println("Current File Map: " + fileMap);
+
+			Iterator<String> it = MapUtil.sortByValue(fileMap).keySet().iterator();
+
+			while (it.hasNext()) {
+				list.add(it.next());
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
+
+	private int findMetadataForSortingCriteria(String criteria, int[] metadata) {
+
+		int metadataValue;
+		if (criteria.equalsIgnoreCase("Words")) {
+			metadataValue = metadata[0];
+		} else if (criteria.equalsIgnoreCase("Letters")) {
+			metadataValue = metadata[1];
+		} else if (criteria.equalsIgnoreCase("Vowels")) {
+			metadataValue = metadata[2];
+		} else {
+			metadataValue = metadata[3];
+		}
+
+		return metadataValue;
+	}
+
+}
---
+++ AceDirectoryScanner/src/com/tjava/Scanner/Utilities/CommonUtilities.java
@@ -0,0 +1,135 @@
+package com.tjava.Scanner.Utilities;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class CommonUtilities {
+
+	public static final String FINAL_CHAR_REGEX = "[!@#$%^&*()[\\\\]|;',./{}\\\\\\\\:\\\"<>?]";
+
+	public static List<File> getAllMTDFiles(String dir) {
+
+		List<File> mtdFile = new ArrayList<File>();
+
+		File directory = new File(dir);
+
+		// get all the files from a directory
+		File[] fList = directory.listFiles();
+
+		for (File file : fList) {
+			if (file.isFile()) {
+				if (file.getAbsolutePath().endsWith(".mtd")) {
+					mtdFile.add(file);
+				}
+			}
+		}
+
+		return mtdFile;
+
+	}
+
+	// count no. of words, alphabets, etc. in a file
+	public static int[] scanFile(File file) throws FileNotFoundException, UnsupportedEncodingException {
+		int words = 0, letters = 0, vowel = 0, specialCharCount = 0;
+
+		// array will contain number of word at position 0, letters at position 1 and so
+		// on.
+		int[] metadataArr = new int[4];
+
+		Scanner in = new Scanner(file);
+
+		in = new Scanner(file);
+
+		while (in.hasNext()) {
+			in.next();
+			words++;
+		}
+
+		metadataArr[0] = words;
+
+		in = new Scanner(file);
+		while (in.hasNextLine()) {
+			String str = in.nextLine();
+			for (int i = 0; i < str.length(); i++) {
+				if (Character.isUpperCase(str.charAt(i))) {
+					letters++;
+					if (isVowel(str.charAt(i))) {
+						vowel++;
+					}
+				}
+				if (Character.isLowerCase(str.charAt(i))) {
+					letters++;
+					if (isVowel(str.charAt(i))) {
+						vowel++;
+					}
+				}
+			}
+		}
+
+		metadataArr[1] = letters;
+		metadataArr[2] = vowel;
+
+		in = new Scanner(file);
+		while (in.hasNextLine()) {
+			specialCharCount = specialCharCount + in.nextLine().split(FINAL_CHAR_REGEX, -1).length - 1;
+		}
+
+		metadataArr[3] = specialCharCount;
+
+		// write metadata to .mtd file for each directory and file
+
+		return metadataArr;
+
+	}
+
+	private static boolean isVowel(char ch) {
+		if ((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+				|| ch == 'O' || ch == 'U')) {
+			return true;
+		}
+		return false;
+	}
+
+	public static int[] scanMTDFile(File file) throws FileNotFoundException {
+
+		int[] metadata = new int[4];
+
+		Scanner sc = new Scanner(file);
+
+		while (sc.hasNext()) {
+			String str = sc.nextLine();
+			String[] str1 = str.split(":");
+			if ((str1[0].substring(0, str1[0].length() - 1).equalsIgnoreCase("Words"))) {
+				metadata[0] = Integer.parseInt(str1[1].substring(1));
+			}
+			if ((str1[0].substring(0, str1[0].length() - 1).equalsIgnoreCase("Letters"))) {
+				metadata[1] = Integer.parseInt(str1[1].substring(1));
+			}
+			if ((str1[0].substring(0, str1[0].length() - 1).equalsIgnoreCase("Vowels"))) {
+				metadata[2] = Integer.parseInt(str1[1].substring(1));
+			} else {
+				if ((str1[0].substring(0, str1[0].length() - 1).equalsIgnoreCase("Special Characters"))) {
+					metadata[3] = Integer.parseInt(str1[1].substring(1));
+				}
+			}
+
+		}
+
+		return metadata;
+	}
+
+	// method to delete cache is expired certain seconds
+	public static void deleteCacheIfExpired(Date currentDate, int seconds) {
+
+		int timeDiff = (int) ((new Date().getTime() - currentDate.getTime()) / 1000);
+
+		if (timeDiff > seconds) {
+			FileCache.clearFileCache();
+		}
+	}
+
+}
